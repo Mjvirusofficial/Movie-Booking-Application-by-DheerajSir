a7c5e8dcd2dc21570fa374ea122490119861c83d
mjvirusofficial <deepakravidas565@gmail.com>
2023-02-26 13:00:44 +0530

    Signup complete

--- src/Feature/Shared/Navbar/Navbar.jsx
+++ src/Feature/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export function Navbar(){
         <nav className=" bd navbar navbar-expand-lg bg-body-tertiary navbar-bg">
   <div className="container-fluid">
     <NavLink className="navbar-brand" to="#">
-      <img className="logo" src={img}/>
+      <img className="logo" src={img} alt='img' />
     </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
