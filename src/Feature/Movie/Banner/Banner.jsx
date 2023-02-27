67b55d3752f28573b799cab1468a28169ecac560
mjvirusofficial <deepakravidas565@gmail.com>
2023-02-27 21:31:06 +0530

    banner

--- src/Feature/Movie/Banner/Banner.jsx
+++ src/Feature/Movie/Banner/Banner.jsx
@@ -2,17 +2,33 @@ import "./Banner.css";
 
 export function Banner() {
     return (
-        <div className='banner'>
-            <h1 className='header2'>BOOK YOUR
-                TICKETS FOR</h1>
-<h1 className="mo">MOVIES, EVENTS, SPORTS</h1>
-            <p className="mov">
+        <>
+        <div className="container ">
+    <div className="row m-3 bx ">
+    <h1 className="r1">BOOK YOUR TICKETS FOR</h1>
+    </div>
+    <div className="row"><h1 className="mo">MOVIES, EVENTS, SPORTS</h1>
+</div>
+<div className="row">
+<p className="mov">
                 Buy movie tickets in advance, find movie times watch trailers,
             </p>
             <p className="mov">
                 Read movie reviews and much more
             </p>
+</div>
         </div>
 
+
+        {/* <div className='banner'>
+            <h1 className='header2'>BOOK YOUR
+                TICKETS FOR</h1>
+<h1 className="mo">MOVIES, EVENTS, SPORTS</h1>
+          
+            <p className="mov">
+                Read movie reviews and much more
+            </p>
+        </div> */}
+</>
     )
 }
